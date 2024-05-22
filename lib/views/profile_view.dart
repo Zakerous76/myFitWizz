f4717fbb7725150a14bf41cd7b2bd474ebe06eac
zakerous <m.z.humayoon@gmail.com>
2024-05-22 13:27:56 +0300

    cleaned code

--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: use_build_context_synchronously
 
-import 'dart:developer';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
