2fb17465e15d1b5941e8eb67bdd717b78fcdf17d
zakerous <m.z.humayoon@gmail.com>
2024-05-22 10:55:08 +0300

    created the deleted file

---
+++ lib/services/cloud_crud/cloud_constants.dart
@@ -0,0 +1,6 @@
+const exercisesCollectionName = "exercises";
+const nameFieldname = "name";
+const bodyPartFieldname = "body_part";
+const categoryFieldname = "category";
+const descriptionFieldname = "description";
+const ytLinkFieldname = "yt_link";
